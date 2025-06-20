8317aaf53263069a0b8dcc47ed28fabad9e4851a
Jaume Cozar Rins <cozar769@gmail.com>
2025-06-20 13:29:24 +0200

    avatar eliminado

--- src/components/home/navBar/navBar.tsx
+++ src/components/home/navBar/navBar.tsx
@@ -1,9 +1,4 @@
 import {
-  Avatar,
-  Dropdown,
-  DropdownDivider,
-  DropdownHeader,
-  DropdownItem,
   Navbar,
   NavbarBrand,
   NavbarCollapse,
@@ -24,29 +19,8 @@ export function NavBarra1(){
           
           </NavbarBrand>
           <div className="flex md:order-2">
-            <Dropdown
-              arrowIcon={false}
-              inline
-              label={
-                <Avatar
-                  alt="User settings"
-                  img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-                  rounded
-                />
-              }
-            >
-              <DropdownHeader>
-                <span className="block text-sm">Bonnie Green</span>
-                <span className="block truncate text-sm font-medium">
-                  name@flowbite.com
-                </span>
-              </DropdownHeader>
-              <DropdownItem>Dashboard</DropdownItem>
-              <DropdownItem>Settings</DropdownItem>
-              <DropdownItem>Earnings</DropdownItem>
-              <DropdownDivider />
-              <DropdownItem>Sign out</DropdownItem>
-            </Dropdown>
+            
+  
             <NavbarToggle />
           </div>
           <NavbarCollapse>
